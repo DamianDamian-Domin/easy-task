852caad44d6e4423e522ee9c1c4ad0d30cf4acab
DamianDamian-Domin <damian.domin334@gmail.com>
2023-11-04 18:11:48 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-10 h-screen bg-teal-600 overflow-auto">
+    <h1 class="text-4xl text-white mb-4"> EasyTask Board </h1>
+     <EasytaskBoard></EasytaskBoard>
+  </div>
+</template>
