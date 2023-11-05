69371af8d7f92badf5c7a4ac7c39564cd3d34866
DamianDamian-Domin <damian.domin334@gmail.com>
2023-11-05 17:20:58 +0100

    added add task, delete task, add column, rename column, delete column, localstorage support

--- components/EasytaskBoard.vue
+++ components/EasytaskBoard.vue
@@ -1,18 +1,24 @@
 <template>
-    <div>
+    <div class="flex py-4 items-start overflow-x-auto gap-4">
         <draggable 
         v-model="columns" 
         group="columns" 
         item-key="id" 
         :animation="150"
         handle=".drag-handle"
-        class="flex flex-row gap-4 overflow-x-auto items-start" 
+        class="flex flex-row gap-4 items-start" 
         >
             <template #item="{element: column}: {element: Column}">
                 <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
                     <header class="font-bold mb-4">
                         <DragHandle color="red"></DragHandle>
-                        {{ column.title }}
+                        <input 
+                        class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5 focus:outline-dashed"
+                        @keyup.enter="($event.target as HTMLInputElement).blur()"
+                        @keydown.backspace="column.title === '' ? columns = columns.filter(c => c.id !== column.id) : null"
+                        type="text"
+                        v-model="column.title"
+                        />
                     </header>
                     <draggable 
                     v-model="column.tasks" 
@@ -23,15 +29,23 @@
                     class="task"
                     >
                         <template #item="{element: task}: {element: Task}">
-                            <EasytaskTask :task="task"></EasytaskTask>
+                            <EasytaskTask 
+                            :task="task" 
+                            @delete="column.tasks = column.tasks.filter(task => task.id !== $event)"></EasytaskTask>
                         </template>
                     </draggable>
                     <footer>
-                        <button class=" text-gray-500">+ Add a Card</button>
+                        <NewTask @add="column.tasks.push($event)"></NewTask>
                     </footer>
                 </div>
             </template>
         </draggable>
+        <button 
+        @click="createColumn()"
+        class="bg-gray-200 whitespace-nowrap p-2 rounded opacity-50"
+        >
+        + New column
+        </button>
     </div>
 </template>
 
@@ -41,7 +55,7 @@ import { nanoid } from 'nanoid'
 import draggable from "vuedraggable"
 
 const alt = useKeyModifier("Alt")
-const columns = ref<Column[]>([
+const columns = useLocalStorage<Column[]>('EasyTask', [
     {
         id: nanoid(),
         title: "Backlog",
@@ -102,20 +116,26 @@ const columns = ref<Column[]>([
     }
 ])
 
+function createColumn() {
+    const column: Column = {
+        id: nanoid(),
+        title: "",
+        tasks: []
+    };
+    columns.value.push(column)
+    nextTick(() => {
+        (document.querySelector(".column:last-of-type .title-input") as HTMLInputElement).focus()
+    })
+}
+
 </script>
 
-<style scoped>
+<style>
+.task > .sortable-drag  {
+    @apply bg-gray-400 !important;
+    transform: translateX(180px) rotate(45deg);
+    transform: skewY(5deg);
+}
 
-/* .sortable-drag .task {
-    background-color: aqua;
-    transform: rotate(5deg);
-}
-.sortable-ghost .task {
-    position: relative;
-}
-.sortable-ghost .task::after{
-    content: "";
-    @apply absolute top-0 bottom-0 left-0 right-0 bg-slate-300 rounded;
-} */
 
 </style>
--- components/EasytaskTask.vue
+++ components/EasytaskTask.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="bg-white p-2 mb-2 rounded shadow-sm flex gap-2 max-w-[250px] " :title="task.createdAt.toLocaleDateString()">
+    <div 
+    class="bg-white p-2 mb-2 rounded shadow-sm flex gap-2 max-w-[250px] focus:bg-gray-400 hover:cursor-pointer focus-visible:outline-none "
+    @focus="focused=true"
+    @blur="focused=false"
+    tabindex="0" 
+    >
         <DragHandle color="blue"></DragHandle>
         <span> {{ task.title }} </span>
     </div>
@@ -7,12 +12,18 @@
 
 <script setup lang="ts">
 import type { Task } from '~/types';
-defineProps<{
+const props = defineProps<{
     task: Task;
 }>()
+const emit = defineEmits<{
+    (e: "delete", payload: string): void
+}>()
+
+const focused = ref(false)
+onKeyStroke("Backspace", (e) => {
+    if(focused.value) {
+        emit("delete", props.task.id)
+    }
+})
 
 </script>
-
-<style scoped>
-
-</style>
